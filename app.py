e617778d3f39f0b79e26d975f7e78c5e0babc7ea
Hassan-632 <hassanibrar632@gmail.com>
2025-02-19 01:35:23 +0500

    Space reduced
    The hosting of the api is critical as there is limited space that causes issues to infer the model.

--- app.py
+++ app.py
@@ -29,36 +29,38 @@ def predict():
         return jsonify({"error": "No image file provided"}), 400
 
     # Load image from request
+    if not request.files["image"]:
+        return jsonify({"error": "Missing value of the file"}), 403
+    
     try:
         image_file = request.files["image"]
         image = np.frombuffer(image_file.read(), np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     except Exception as e:
-        return jsonify({"error": e}), 400
+        return jsonify({"error": e}), 403
 
     # Run YOLO inference
     try:
         print('Infering Image for results')
-        # results = model(image)
+        results = model(image)
     except Exception as e:
-        return jsonify({"error": "No image file provided"}), 200
-
-    return jsonify({"message": "Nothing detected"}), 200
+        return jsonify({"error": f"unable to process image. {e}"}), 500
     
-    # for result in results:
-    #     for box, conf, cls in zip(result.boxes.xyxy.cpu().numpy(), 
-    #                               result.boxes.conf.cpu().numpy(), 
-    #                               result.boxes.cls.cpu().numpy()):
-    #         detections.append({
-    #             "class": model.names[int(cls)],
-    #             "confidence": float(conf),
-    #             "box": [int(box[0]), int(box[1]), int(box[2]), int(box[3])]
-    #         })
+    detections = []
+    for result in results:
+        for box, conf, cls in zip(result.boxes.xyxy.cpu().numpy(), 
+                                  result.boxes.conf.cpu().numpy(), 
+                                  result.boxes.cls.cpu().numpy()):
+            detections.append({
+                "class": model.names[int(cls)],
+                "confidence": float(conf),
+                "box": [int(box[0]), int(box[1]), int(box[2]), int(box[3])]
+            })
 
-    # return jsonify({
-    #     "message": "Inference completed successfully",      # Retrun Success Message
-    #     "detections": detections                           # Return Dtections
-    # }), 200
+    return jsonify({
+        "message": "Inference completed successfully",      # Retrun Success Message
+        "detections": detections                           # Return Dtections
+    }), 200
 
 if __name__ == "__main__":
     app.run()
